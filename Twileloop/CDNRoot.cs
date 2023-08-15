e986477c1c77c2ad4f2b9cb63f45eb1ab604bbfd
Sangeeth Nandakumar <twileloop@outlook.com>
2023-08-16 03:49:10 +0530

    fix - Changed CDN roots

--- Twileloop/CDNRoot.cs
+++ Twileloop/CDNRoot.cs
@@ -2,7 +2,13 @@
 {
     public static class CDNRoot
     {
-        public const string SHARED = "https://cdn.jsdelivr.net/gh/sangeethnandakumar/CDNRack@master/shared";
-        public const string PROJECT = "https://cdn.jsdelivr.net/gh/sangeethnandakumar/CDNRack@master/twileloop";
+        //public const string SHARED = "https://cdn.jsdelivr.net/gh/sangeethnandakumar/CDNRack@master/shared";
+        //public const string PROJECT = "https://cdn.jsdelivr.net/gh/sangeethnandakumar/CDNRack@master/twileloop";
+        
+        public const string BASE = "https://localhost:7000";
+        //public const string BASE = "https://twileloop.com";
+
+        public const string SHARED = $"{BASE}/cdn/shared";
+        public const string PROJECT = $"{BASE}/cdn/project";
     }
 }
