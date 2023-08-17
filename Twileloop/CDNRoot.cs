b68538c3b7a229f0ffaa405569a1f5b7a5bce964
Sangeeth Nandakumar <twileloop@outlook.com>
2023-08-17 07:38:11 +0530

    fix - Site updates

--- Twileloop/CDNRoot.cs
+++ Twileloop/CDNRoot.cs
@@ -2,12 +2,7 @@
 {
     public static class CDNRoot
     {
-        //public const string SHARED = "https://cdn.jsdelivr.net/gh/sangeethnandakumar/CDNRack@master/shared";
-        //public const string PROJECT = "https://cdn.jsdelivr.net/gh/sangeethnandakumar/CDNRack@master/twileloop";
-        
-        public const string BASE = "";
-
-        public const string SHARED = $"{BASE}/cdn/shared";
-        public const string PROJECT = $"{BASE}/cdn/project";
+        public const string SHARED = $"/cdn/shared";
+        public const string PROJECT = $"/cdn/project";
     }
 }
